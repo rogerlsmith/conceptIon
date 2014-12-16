f835a4eab2091528bc23f36b03bc19373a887197
Roger Smith <rogerlsmith@live.com>
2014-12-15 22:57:25 -0500

    big changes & fixes

--- js/app.js
+++ js/app.js
@@ -1,38 +1,44 @@
-// Ionic Starter App
 
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
-// 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers'])
+//bower install ngCordova
 
-.run(function($ionicPlatform) {
-  
-  $ionicPlatform.ready(function() {
+angular.module('starter', ['ionic', 'ngCordova', 'starter.controllers'])
+
+.run ( function ( $ionicPlatform ) {
+
+  $ionicPlatform.ready ( function ( ) {
+
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
-    if (window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+
+    if ( window.cordova && window.cordova.plugins.Keyboard ) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar ( true );
     }
-    if (window.StatusBar) {
+    if ( window.StatusBar ) {
       // org.apache.cordova.statusbar required
-      StatusBar.styleDefault();
+      StatusBar.styleDefault ( );
     }
   });
-})
+})    // end of run
 
-.config(function($stateProvider, $urlRouterProvider) {
+.config ( function ( $stateProvider, $urlRouterProvider ) {
+
+  var showMenu = "templates/userLogin.html";
+
+  var loggedin = false;
+  if ( loggedin ) {
+    showMenu = "templates/userMenu.html";
+  }
+
   $stateProvider
 
-  .state('app', {
+  .state ( 'app', {
     url: "/app",
     abstract: true,
-    templateUrl: "templates/menu.html",
+    templateUrl: showMenu,
     controller: 'AppCtrl'
-  })
+  } )
 
-
-  .state('app.search', {
+  .state ( 'app.search', {
     url: "/search",
     views: {
       'menuContent': {
@@ -41,7 +47,7 @@ angular.module('starter', ['ionic', 'starter.controllers'])
     }
   })
 
-  .state('app.browse', {
+  .state ( 'app.browse', {
     url: "/browse",
     views: {
       'menuContent': {
@@ -49,17 +55,18 @@ angular.module('starter', ['ionic', 'starter.controllers'])
       }
     }
   })
-    .state('app.playlists', {
-      url: "/playlists",
-      views: {
-        'menuContent': {
-          templateUrl: "templates/playlists.html",
-          controller: 'PlaylistsCtrl'
-        }
-      }
-    })
 
-  .state('app.single', {
+  .state ( 'app.playlists', {
+    url: "/playlists",
+    views: {
+      'menuContent': {
+        templateUrl: "templates/playlists.html",
+        controller: 'AudioCtrl'
+      }
+    }
+  })
+
+  .state ( 'app.single', {
     url: "/playlists/:playlistId",
     views: {
       'menuContent': {
@@ -71,5 +78,8 @@ angular.module('starter', ['ionic', 'starter.controllers'])
 
 
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/app/playlists');
-});
+  $urlRouterProvider.otherwise ( '/app/playlists' );
+
+});   // end of config
+
+
--- js/controllers.js
+++ js/controllers.js
@@ -1,100 +1,188 @@
+
+
 angular.module ( 'starter.controllers', [] )
 
-.controller ( 'AppCtrl', function ( $scope, $ionicModal, $timeout ) {
+
+.controller ( 'AppCtrl', function ( $scope, $http, $ionicModal, $timeout ) {
+
+
+/* Login */
+
 
   // Form data for the login modal
   $scope.loginData = {};
-
 
   // Create the login modal that we will use later
   $ionicModal.fromTemplateUrl ( 'templates/login.html', {
     scope: $scope
   }).then ( function ( modal ) {
-
-    $scope.modalLogin = modal;
-
+    $scope.loginmodal = modal;
   });
 
   // Triggered in the login modal to close it
   $scope.closeLogin = function ( ) {
-
-    $scope.modalLogin.hide ( );
-
+    $scope.loginmodal.hide ( );
   };
-
 
   // Open the login modal
   $scope.login = function ( ) {
-
-    $scope.modalLogin.show();
-
+    $scope.loginmodal.show ( );
   };
-
 
   // Perform the login action when the user submits the login form
   $scope.doLogin = function ( ) {
-    console.log ( 'Doing login', $scope.loginData );
+    console.log ( 'Doing login: ', $scope.loginData );
 
-    // Simulate a login delay. Remove this and replace with your login
-    // code if using a login system
-    $timeout ( function ( ) {
+    $http.post ( 'http://rogerlsmith.net/concept/bower_components/bootstrap/mobile/user.php', {
+          username: $scope.loginData.username,
+          password: $scope.loginData.password,
+          method: 'login'
+      }).
+      success ( function ( data, status, headers, config ) {
+          alert ( "success" );
 
-      $scope.closeLogin ( );
-    }, 1000 );
-
+      }).
+      error ( function ( data, status, headers, config ) {
+          alert ( "Error" );
+      });
   };
 
 
-  // Form data for the Register modal
+
+/* Register */
+
+
+  // Form data for the register modal
   $scope.registerData = {};
 
-  // Create the Register modal that we will use later
-  $ionicModal.fromTemplateUrl ( 'templates/register.html', {
+  // Create the register modal that we will use later
+  $ionicModal.fromTemplateUrl('templates/register.html', {
     scope: $scope
   }).then ( function ( modal ) {
-
-    $scope.modalRegister = modal;
-
+    $scope.registermodal = modal;
   });
 
-
-  // Triggered in the Register modal to close it
+  // Triggered in the register modal to close it
   $scope.closeRegister = function ( ) {
-
-    $scope.modalRegister.hide ( );
-
+    $scope.registermodal.hide ( );
   };
 
-
-  // Open the Register modal
+  // Open the register modal
   $scope.register = function ( ) {
-
-    $scope.modalRegister.show ( );
-
+    $scope.registermodal.show ( );
   };
 
-
-  // Perform the Register action when the user submits the Register form
+  // Perform the register action when the user submits the register form
   $scope.doRegister = function ( ) {
-    console.log ( 'Doing register', $scope.registerData );
+    console.log('Doing register', $scope.registerData);
 
-    // Simulate a Register delay. Remove this and replace with your REgister
-    // code if using a Register system
-    $timeout ( function ( ) {
+    $http.post ( 'http://rogerlsmith.net/concept/bower_components/bootstrap/mobile/user.php', {
+          username: $scope.registerData.username,
+          firstname: $scope.registerData.firstname,
+          lastname: $scope.registerData.lastname,
+          email: $scope.registerData.email,
+          password: $scope.registerData.password,
+          method: 'register'
+      }).
+      success ( function ( data, status, headers, config ) {
+          alert ( "success" );
+      }).
+      error ( function ( data, status, headers, config ) {
+          alert ( "Error" );
+      });
+    };
 
-      $scope.closeRegister ( );
-    }, 1000 );
+
+/* Forgot Password */
+
+
+      // Form data for the forgot password modal
+    $scope.forgotData = {};
+
+    // Create the forgot password modal that we will use later
+    $ionicModal.fromTemplateUrl ( 'templates/forgot.html', {
+      scope: $scope
+    }).then( function ( modal ) {
+      $scope.forgotmodal = modal;
+    });
+
+    // Triggered in the forgot modal to close it
+    $scope.closeForgot = function ( ) {
+      $scope.forgotmodal.hide ( );
+    };
+
+    // Open the forgot password modal
+    $scope.forgotPassword = function ( ) {
+      $scope.forgotmodal.show ( );
+    };
+
+    // Perform the register action when the user submits the register form
+    $scope.doForgot = function() {
+      console.log ( 'Doing forgot password', $scope.forgotData );
+
+
+      $http.post ( '/forgot', {
+          username: $scope.forgotData.username,
+          email: $scope.forgotData.email
+        }).
+        success( function ( data, status, headers, config ) {
+            alert ( "success" );
+        }).
+        error( function ( data, status, headers, config ) {
+            alert( "Error" );
+        });
+    };
+
+
+    // $scope
+    //   .uploadFile ( 'http://rogerlsmith.net/concept/bower_components/bootstrap/mobile/audio.php', 
+    //     'test.mp3', 
+    //     options )
+    //   .then( function ( result ) {
+    //      //Success
+    //   }, function ( err ) {
+    //       //Error
+    //   }, function ( progress ) {
+    //       //Constant Progress updates
+    //   });
+
+
+
+    // $scope.captureAudio = function() {
+    //   var options = { limit: 3, duration: 10 };
+
+    //   $cordovaCapture.captureAudio(options).then(function(audioData) {
+    //     // Success! Audio data is here
+    //   }, function(err) {
+    //     // An error occurred. Show a message to the user
+    //   });
+
+  })        // end of AppCtrl
+
+
+.controller ( 'AudioCtrl', function ( $scope ) {
+
+  $scope.playlists = [
+    { title: 'Reggae', id: 1 },
+    { title: 'Chill', id: 2 },
+    { title: 'Dubstep', id: 3 },
+    { title: 'Indie', id: 4 },
+    { title: 'Rap', id: 5 },
+    { title: 'Cowbell', id: 6 }
+  ];
+
+  $scope.showPublic = function ( ) {
 
   };
 
+  $scope.showPrivate = function ( ) {
+
+  };
+
+})        // end of AudioCtrl
 
 
-});
+.controller('PlaylistCtrl', function($scope, $stateParams) {
+});       // end of PlaylistCtrl
 
-.controller ( 'PlaylistsCtrl', function ( $scope ) {
-  $scope.playlists = [];
-})
 
-.controller ( 'PlaylistCtrl', function ( $scope, $stateParams ) {
-
-});
